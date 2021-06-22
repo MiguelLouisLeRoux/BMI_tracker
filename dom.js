1c113f0a53e77a2538992b1578b1942c6eb61e25
MiguelLouisLeRoux <miguellerouxleroux@gmail.com>
2021-06-22 16:45:00 +0200

    added template

--- dom.js
+++ dom.js
@@ -2,5 +2,5 @@ const ctr = document.getElementById("theChart").getContext('2d');
 const factoryLogic = factory();
 
 let graph = new Chart (ctr, {
-
+    
 });
--- tests/factory.js
+++ tests/factory.js
@@ -1,11 +1,12 @@
 function factory() {
     let invitationList = [{Name: "Pete", going: 'yes'}, {Name: "Ben", going: 'yes'},{Name: "Charles", going: 'no'},{Name: "Sally", going: 'no'},{Name: "Polly", going: 'yes'},{Name: "Paige", going: 'no'},{Name: "Michael", going: 'no'},{Name: "Amber", going: 'no'},{Name: "Zena", going: 'yes'}];
     
-    
+    let venueCap;
 
     function values(){
         return{
             invitationList: invitationList,
+            venueCap: venueCap,
         }
     }
 
