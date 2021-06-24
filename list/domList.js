6a42e9ff5c8951710d1e607429fce1da65b21c56
MiguelLouisLeRoux <miguellerouxleroux@gmail.com>
2021-06-24 11:09:21 +0200

    added the add section to second page

--- list/domList.js
+++ list/domList.js
@@ -1,4 +1,28 @@
 const ctr = document.getElementById("theChart").getContext('2d');
+const nameIn = document.querySelector(".nameVal");
+const theAdd = document.querySelector(".add");
+const errorMes = document.querySelector(".errorr");
+
+const theFactory = factory();
+
+let vac = 100;
+let occ = 0;
+
+if (localStorage["theList"] && localStorage["maxCap"] && localStorage["occupied"]) {
+    occ = Number(localStorage["occupied"]);
+    vac = Number(localStorage["maxCap"]);
+    let theListt = JSON.parse(localStorage["theList"]);
+
+    theFactory.resettingLocalStorage(occ, vac, theListt);
+    // limit.innerHTML = factoryLogic.values().allowedCap;
+}
+
+if (localStorage["maxCap"]) {
+    vac = Number(localStorage["maxCap"]);
+
+    theFactory.resetVac(vac);
+    // limit.innerHTML = factoryLogic.values().allowedCap;
+}
 
 Chart.defaults.font.size = 20;
 Chart.defaults.font.color = 'black';
@@ -10,22 +34,14 @@ let graph = new Chart (ctr, {
         datasets: [{
             label: '# of Votes',
             fontWeight: 'bold',
-            data: [0, 100],
+            data: [occ, vac],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
-                // 'rgba(54, 162, 235, 0.2)',
                 'rgba(255, 206, 86, 0.2)',
-                // 'rgba(75, 192, 192, 0.2)',
-                // 'rgba(153, 102, 255, 0.2)',
-                // 'rgba(255, 159, 64, 0.2)'
             ],
             borderColor: [
                 'rgba(255, 99, 132, 1)',
-                // 'rgba(54, 162, 235, 1)',
                 'rgba(255, 206, 86, 1)',
-                // 'rgba(75, 192, 192, 1)',
-                // 'rgba(153, 102, 255, 1)',
-                // 'rgba(255, 159, 64, 1)'
             ],
             borderWidth: 3,
             
@@ -39,3 +55,48 @@ let graph = new Chart (ctr, {
         }
     }
 });
+
+theAdd.addEventListener('click', function(){
+
+    if (nameIn.value === "") {
+        nameIn.classList.add("crimson");
+        nameIn.value = "Please enter name"
+        setTimeout(function(){
+            nameIn.value = "";
+            nameIn.classList.remove("crimson");
+        }, 1500);
+
+    } else if (theFactory.values().allowedCap === undefined) {
+
+        // theError.innerHTML = factoryLogic.values().error;
+        errorMes.innerHTML = "An event has not been set yet.";
+        setTimeout(function(){
+            errorMes.innerHTML = "";
+        }, 1500);
+        nameIn.value = "";
+
+    } else if (theFactory.values().allowedCap === 0) {
+    
+        errorMes.innerHTML = "Maximum capacity reached";
+        setTimeout(function(){
+            errorMes.innerHTML = "";
+        }, 1500);
+        nameVal.value = "";
+
+    } else {
+        theFactory.addToList(nameIn.value);
+        
+        // limit.innerHTML = theFactory.values().allowedCap;
+
+        localStorage["theList"] = JSON.stringify(theFactory.values().goingList);
+        localStorage["maxCap"] = theFactory.values().allowedCap;
+        localStorage["occupied"] = theFactory.values().occupied;
+
+        nameIn.value = "";
+        location.reload();
+    }
+});
+
+function removeBtn(val) {
+
+};
